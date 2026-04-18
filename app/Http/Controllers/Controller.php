4e8276c5decdd8e16f993588fcdd06c13ca8a0c4
gabrielroot <gabrielfer.s88@gmail.com>
2026-04-18 09:10:44 -0300

    refactor: simplify response handling in TranslatorController by utilizing buildApiResponse method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\IntegrationResponse;
+
 abstract class Controller
 {
-    //
+    protected function buildApiResponse(IntegrationResponse $response): \Illuminate\Http\JsonResponse
+    {
+        if ($response->isSuccess()) {
+            return response()->json(
+                $response->getData(),
+                $response->getHttpStatusCode()
+            );
+        }
+
+        return response()->json(
+            ['message' => $response->getMessage(), 'meta' => $response->getMeta()],
+            $response->getHttpStatusCode() ?? 500
+        );
+    }
 }
--- app/Http/Controllers/TranslatorController.php
+++ app/Http/Controllers/TranslatorController.php
@@ -9,48 +9,18 @@ class TranslatorController extends Controller
     public function getLanguages(TranslationService $translationService) {
         $response = $translationService->getLanguages();
 
-        if ($response->isSuccess()) {
-            return response()->json(
-                $response->getData(),
-                $response->getHttpStatusCode()
-            );
-        }
-
-        return response()->json(
-            ['message' => $response->getMessage(), 'meta' => $response->getMeta()],
-            $response->getHttpStatusCode() ?? 500
-        );
+        return $this->buildApiResponse($response);
     }
 
     public function detectLanguage(TranslationService $translationService) {
         $response = $translationService->detectLanguage(request()->all());
 
-        if ($response->isSuccess()) {
-            return response()->json(
-                $response->getData(),
-                $response->getHttpStatusCode()
-            );
-        }
-
-        return response()->json(
-            ['message' => $response->getMessage(), 'meta' => $response->getMeta()],
-            $response->getHttpStatusCode() ?? 500
-        );
+        return $this->buildApiResponse($response);
     }
 
     public function translate(TranslationService $translationService) {
         $response = $translationService->translate(request()->all());
 
-        if ($response->isSuccess()) {
-            return response()->json(
-                $response->getData(),
-                $response->getHttpStatusCode()
-            );
-        }
-
-        return response()->json(
-            ['message' => $response->getMessage(), 'meta' => $response->getMeta()],
-            $response->getHttpStatusCode() ?? 400
-        );
+        return $this->buildApiResponse($response);
     }
 }
